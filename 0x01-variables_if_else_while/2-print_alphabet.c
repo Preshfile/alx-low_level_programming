cc94c38c427e8023d16d0ce61d7c8d0791ec79be
Preshfile <preshcharlies2014@gmail.com>
2022-07-01 19:06:32 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; leteter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
