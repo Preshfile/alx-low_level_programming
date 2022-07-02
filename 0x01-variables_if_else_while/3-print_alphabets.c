76a8c9e0837b487dd9b2940ffba8492ff5dae8ac
Preshfile <preshcharlies2014@gmail.com>
2022-07-01 19:34:14 -0700

    Print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,17 @@
 int main(void)
 {
 	char letter;
+	
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		putchar(letter);
+	}
 
 	for (letter = 'A'; letter <= 'z'; letter++)
+	{
 		putchar(letter);
+	}
+
 	putchar('\n');
 
 	return (0);
