eed4531ba4817b7b3f66e3f833ac4f8d87cb9a6b
Preshfile <preshcharlies2014@gmail.com>
2022-07-26 17:57:54 -0700

    This is a function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -37,3 +37,20 @@ char *argstostr(int ac, char **av)
 	}
 	return (new_string);
 }
+
+/**
+ * len - returns length of str
+ * @str: string counted
+ * Return: returns the length
+ */
+int len(char *str)
+{
+	int len = 0;
+
+	if (str != NULL)
+	{
+		while (str[len])
+			len++;
+	}
+	return (len);
+}
